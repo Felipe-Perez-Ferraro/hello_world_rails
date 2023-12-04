9119ad5509a61d5b53fb8aa0556b9db0df9d1bae
felipe <fperezferraro@gmail.com>
2023-12-04 18:05:35 -0300

    Initialize project

---
+++ app/javascript/application.js
@@ -0,0 +1,17 @@
+import "@hotwired/turbo-rails"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+  <>
+    <h1>Hello Felipe, como e</h1>
+    <h1>Hello Felipe, como e</h1>
+  </>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
