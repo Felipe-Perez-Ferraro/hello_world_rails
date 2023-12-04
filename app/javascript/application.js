199f1cf451a95ceb2ebc2d47fcb06a236f3a2a1e
felipe <fperezferraro@gmail.com>
2023-12-04 20:25:52 -0300

    Created the root path and the store provider

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,32 +1,30 @@
-import "@hotwired/turbo-rails"
-import React, { useState, useEffect } from 'react';
+import '@hotwired/turbo-rails';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Greeting from './components/Greeting';
+import store from './redux/store';
 
 function App() {
-  const [data, setData] = useState([])
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <>
+          <Greeting />
+        </>
+      ),
+    },
+  ]);
 
-  useEffect(() => {
-    const fetchApi = async () => {
-      try {
-        const response = await fetch('http://127.0.0.1:3000/api/v1/messages')
-        const result = await response.json()
-        setData(result)
-        console.log(result)
-      } catch (error) {
-        throw new Error(error)
-      }
-    }
-
-    fetchApi()
-  }, [])
   return (
     <>
-      <h1>{data.message}</h1>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </>
   );
 }
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
