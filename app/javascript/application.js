ea7254e11a8b0ecc184454f874627f06eec28238
felipe <fperezferraro@gmail.com>
2023-12-04 18:06:06 -0300

    Initialize project

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,12 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  return (
-  <>
-    <h1>Hello Felipe, como e</h1>
-    <h1>Hello Felipe, como e</h1>
-  </>
-  );
+  return (<h1>Hello World</h1>);
 }
 
 ReactDOM.render(
