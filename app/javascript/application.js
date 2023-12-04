97706131aa40b6d9d4b168f72fad0848f451644b
felipe <fperezferraro@gmail.com>
2023-12-04 19:03:13 -0300

    Created the application logic

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,29 @@
 import "@hotwired/turbo-rails"
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  return (<h1>Hello World</h1>);
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:3000/api/v1/messages')
+        const result = await response.json()
+        setData(result)
+        console.log(result)
+      } catch (error) {
+        throw new Error(error)
+      }
+    }
+
+    fetchApi()
+  }, [])
+  return (
+    <>
+      <h1>{data.message}</h1>
+    </>
+  );
 }
 
 ReactDOM.render(
